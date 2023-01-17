d271207ecbc633a844a5bf9a923f4a1a8e03518f
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-01-17 22:57:46 +0600

    what we do added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../HeroSection/Hero';
+import WhatWeDo from '../WhatWeDo/WhatWeDo';
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <WhatWeDo></WhatWeDo>
     </div>
   );
 };
---
+++ src/Pages/Home/WhatWeDo/WhatWeDo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './WhatWeDo.css'
+
+const WhatWeDo = () => {
+    return (
+        <div className='wh-we-do'>
+            <p className='sub-title'>
+                What We Do?
+            </p>
+            <h2 className='title'>
+                <span className='heighlight-words'>Interactive</span> Website with <span className='heighlight-words'>Creative</span>
+                <br />UI design and Web app development agency
+            </h2>
+            <p className='details'>
+                In order to serve better, we create a completer web design and development package for our customers. Most of the top rated companies are already affiliated with us. We work with <span>great ideas</span>, <span>creative people</span> and <span>new technologies</span>. Also keep an eye out for
+                customer feedback and support to keep use updated.
+            </p>
+        </div>
+    );
+};
+
+export default WhatWeDo;
