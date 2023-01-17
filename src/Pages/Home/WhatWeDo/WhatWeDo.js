3bd6c23d827e2ce96d3ff993c2cf8b49b0d67d24
kali linux <error@linux.linux>
2023-01-17 23:21:16 +0600

    1121pm

--- src/Pages/Home/WhatWeDo/WhatWeDo.js
+++ src/Pages/Home/WhatWeDo/WhatWeDo.js
@@ -4,15 +4,16 @@ import './WhatWeDo.css'
 const WhatWeDo = () => {
     return (
         <div className='wh-we-do'>
-            <p className='sub-title'>
+            <p className='subTitle'>
                 What We Do?
             </p>
-            <h2 className='title'>
-                <span className='heighlight-words'>Interactive</span> Website with <span className='heighlight-words'>Creative</span>
-                <br />UI design and Web app development agency
+            <h2 className='title2'>
+                <span className='highlight_word'>Interactive </span>
+                 Website with <span className='highlight_word'>Creative </span><br/>
+                UI design and Web app development agency
             </h2>
-            <p className='details'>
-                In order to serve better, we create a completer web design and development package for our customers. Most of the top rated companies are already affiliated with us. We work with <span>great ideas</span>, <span>creative people</span> and <span>new technologies</span>. Also keep an eye out for
+            <p className='para1 topGaps5'>
+                In order to serve better, we create a completer web design and development package for our customers. Most of the top rated companies are already affiliated with us. We work with <b>great ideas</b>, <b>creative people</b> and <b>new technologies</b>. Also keep an eye out for
                 customer feedback and support to keep use updated.
             </p>
         </div>
--- src/Pages/Portfolio/Work/Work.js
+++ src/Pages/Portfolio/Work/Work.js
@@ -4,7 +4,7 @@ import './Work.css';
 const Work = () => {
   return (
     <div className='work_div'>
-      <h1 className='title1'>What we have done?</h1>
+      <h1 className='title2'>What we have done?</h1>
       <div className='folio_div'>
         <div>
 
