851a57e1e94a6d74faae4876503d62746797ed9e
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-01-17 21:29:59 +0600

    hero updated

--- src/Pages/Home/HeroSection/Hero.js
+++ src/Pages/Home/HeroSection/Hero.js
@@ -10,25 +10,21 @@ const Hero = () => {
                     HOSTING AND USE BEST MARKETING<br />
                     STRATEGY
                 </h1>
-                <div className='underline w-1/5 bg-white h-1'>
 
-                </div>
-                <div className='grid md:grid-cols-2 grid-cols-1 2xl:mt-10'>
+                <div className='underline'></div>
+
+                <div className='hero-detail'>
                     <div>
-                        <p className='pr-16 mb-12 2xl:text-3xl 2xl:leading-normal'>
+                        <p className='hero-para'>
                             We will design your website with the best options and help to complete our full services.
                         </p>
-                        <div className='flex items-center justify-center 2xl:text-xl md:justify-start 2xl:mt-20'>
-                            <Link className='mr-10'>
-                                <button className="portfoliobtnBg hover:bg-transparent font-semibold hover:bg-white hover:text-blue-500 2xl:py-3 2xl:px-6 py-2 px-4 rounded">
+                        <div className='hero-links'>
+                            <Link className="primary-btn">
                                     Portfolio
-                                </button>
                             </Link>
-                            <p className='mr-10'>or</p>
-                            <Link>
-                                <button className="bg-white 2xl:py-3 2xl:px-6 2xl:text-xl contactbtn font-semibold hover:bg-blue-500 py-2 px-4 btnBorder rounded">
+                            <p>or</p>
+                            <Link className="hero-contact">
                                     Contact Us
-                                </button>
                             </Link>
                         </div>
                     </div>
