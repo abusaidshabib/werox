4ee06bf26aec27c86adbe6f502cce83bf5dc1440
kali linux <error@linux.linux>
2023-01-17 21:29:03 +0600

    our work content

---
+++ src/Pages/Portfolio/Work/Work.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Work.css';
+
+const Work = () => {
+  return (
+    <div className='work_div'>
+      <h1>What we have done?</h1>
+      <div className='folio_div'>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
